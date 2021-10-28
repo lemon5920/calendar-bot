a77ead3100afdae34e3172e7682a8ea77b3acb8f
lemon5920 <lemon5920@gmail.com>
2021-10-28 23:26:42 +0800

    feat: Add Messenger generic template message.

---
+++ src/Messenger/index.js
@@ -0,0 +1,32 @@
+module.exports = async function MessengerAction(context) {
+  if (context.event.isText) {
+    await context.sendGenericTemplate([
+      {
+        title: "Welcome to Peter's Hats",
+        imageUrl: "https://picsum.photos/300/200",
+        subtitle: "We've got the right hat for everyone.",
+        defaultAction: {
+          type: "web_url",
+          url: "https://yahoo.com/",
+          webviewHeightRatio: "tall",
+        },
+        buttons: [
+          {
+            type: "web_url",
+            title: "Google Webview",
+            url: "https://google.com/",
+            messengerExtensions: true,
+            webviewHeightRatio: "tall",
+            fallbackUrl: "https://google.com/",
+          },
+          {
+            type: "web_url",
+            title: "Google Link",
+            url: "https://google.com/",
+            webviewHeightRatio: "tall",
+          },
+        ],
+      },
+    ]);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const { platform, router } = require("bottender/router");
 const SlackAction = require("./Slack");
+const MessengerAction = require("./Messenger");
 
 module.exports = async function App() {
-  return router([platform("slack", SlackAction)]);
+  return router([
+    platform("slack", SlackAction),
+    platform("messenger", MessengerAction),
+  ]);
 };
